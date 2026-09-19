a107be366d792614c44dc57b842628343452a5ca
Embruh69 <93874399+Embruh69@users.noreply.github.com>
2026-09-19 13:48:32 +0700

    Updated beta test time

--- assets/js/countdown-gate.js
+++ assets/js/countdown-gate.js
@@ -17,7 +17,7 @@
 (function () {
   'use strict';
 
-  var TARGET = new Date('2026-09-19T13:47:00+07:00');
+  var TARGET = new Date('2026-09-19T13:54:00+07:00');
     window.COUNTDOWN_TARGET = TARGET;
 
   var FONT = {
